f8e8b249874c8c8f66e0ba4bd3f7bf14620b9f88
a-cuc <148326642+a-cuc@users.noreply.github.com>
2024-10-19 10:08:05 +0800

    Update test.cpp

--- Midterm/Sensor-based Data Gathering/test.cpp
+++ Midterm/Sensor-based Data Gathering/test.cpp
@@ -1,73 +1,84 @@
+#include <WiFi.h>
+#include "time.h"
 #include "DHT.h"
+#include "ThingSpeak.h"
 
-#define DHTPIN 2
-#define DHTTYPE DHT22
+WiFiClient client;
+const char *ssid = "pc-server";
+const char *password = "password";
 
-int counter = 0;
-float tempvals[10];
-float temphumid[10];
-float AverageHumidity = 0;
-float AverageTemperature = 0;
 
+#define DHTPIN 26  
+#define DHTTYPE DHT22 
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-  Serial.begin(9600);
-  //to add location column
-  //Serial.println("Humidity, \tTemp_C, \tLocation");
-  Serial.println("Humidity, \tTemp_C");
+float humidityReadings[10];
+float temperatureReadings[10];
+int readingIndex = 0;
 
-  dht.begin();
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  dht.begin();  
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");;
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  ThingSpeak.begin(client);
+
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(60000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    digitalWrite(2, LOW);
+    if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wi-Fi disconnected! Reconnecting...");
+    WiFi.reconnect();
     return;
   }
 
-  //Serial.print(h);
-  //Serial.print(", \t");
-  //Serial.print(t);
-
-  tempvals[counter] = t;
-  temphumid[counter] = h;
-  counter++;
-
-  if (counter == 10) {
+  static unsigned long lastReadingTime = 0;
+  if (millis() - lastReadingTime >= 60000 ) { // time in between reads
+    lastReadingTime = millis();
     
-    for (int i = 0; i < 10; i++) 
-    {
-      AverageHumidity += temphumid[i];
-      AverageTemperature += tempvals[i];
-    }
-    AverageHumidity /= 10;
-    AverageTemperature /= 10;
-    //Serial.print("Average Humidity is:");
-    Serial.print(AverageHumidity);
-    Serial.print(", \t");
-    //Serial.print(", \tAverage Temperature is:");
-    Serial.print(AverageTemperature);
-    Serial.println();
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
 
-    memset(tempvals, 0, 10);
-    memset(temphumid, 0, 10);
-    AverageHumidity = 0;
-    AverageTemperature = 0;
-    counter = 0;
+    humidityReadings[readingIndex] = humidity;
+    temperatureReadings[readingIndex] = temperature;
+    readingIndex++;
+    // you can add code for printing per minute temp & humid here
+    Serial.print(temperature);
+    Serial.print(", \t");
+    Serial.print(humidity);
+    Serial.print(", \t");
+    Serial.println();
   }
-    //Code added is for Location
-    //Serial.print(", \t");
-    //Serial.print("Study_Area");
-    //Serial.println();
+
+  if (readingIndex == 10) {
+    float avgHumidity = 0.0;
+    float avgTemperature = 0.0;
+    for (int i = 0; i < 10; i++) {
+      avgHumidity += humidityReadings[i];
+      avgTemperature += temperatureReadings[i];
+    }
+    avgHumidity /= 10;
+    avgTemperature /= 10; 
+    ThingSpeak.setField(1, avgHumidity);
+    ThingSpeak.setField(2, avgTemperature);
+    int x = ThingSpeak.writeFields(channel, API);
+    if (x == 200) {
+      digitalWrite(2, HIGH);
+    }
+
+    readingIndex = 0;
+  }
+  delay(1000);
+  
 }
